48b122f8030ddcde3eecaf98e17e86dd40883ec0
Yu <yu212.mc@gmail.com>
2023-10-24 00:01:02 +0900

    add commands

--- boonbot/chall.py
+++ boonbot/chall.py
@@ -1,28 +1,68 @@
+import enum
+
 import discord
+from discord import Thread
+
 from .main import tree
 from .config import config
 
 
-@tree.command(
-    name="new-chall",
-    description="新しい問題スレッドを作成します"
-)
-@discord.app_commands.choices(
-    category=[
-        discord.app_commands.Choice(name="Pwnable", value="pwn"),
-        discord.app_commands.Choice(name="Crypto", value="crypto"),
-        discord.app_commands.Choice(name="Web", value="web"),
-        discord.app_commands.Choice(name="Reversing", value="rev")
-    ]
-)
-async def new_chall(ctx: discord.Interaction, category: str, problem_name: str):
-    channel_name = f"{category}-{problem_name}"
-    thread = await ctx.channel.create_thread(name=channel_name, reason="あああ")
-    await ctx.response.send_message(f"スレッドを作成したよ！: {thread.mention}")
+SOLVED_POSTFIX = " (solved)"
 
 
-@tree.command(
-    name="solved"
-)
+class Categories(enum.Enum):
+    WEB = "web"
+    CRYPTO = "crypto"
+    REVERSING = "rev"
+    FORENSIC = "forensic"
+    PWNABLE = "pwn"
+    MISC = "misc"
+    BLOCKCHAIN = "blockchain"
+    OSINT = "osint"
+    PROGRAMMING = "ppc"
+
+
+@tree.command(name="new-chall")
+async def new_chall(ctx: discord.Interaction, category: Categories, problem_name: str):
+    channel_name = f"{category.name}: {problem_name}"
+    for channel in ctx.channel.threads:
+        if channel.name.removesuffix(SOLVED_POSTFIX) == channel_name:
+            await ctx.response.send_message(f"すでに存在するよ！", ephemeral=True)
+            return
+    thread = await ctx.channel.create_thread(name=channel_name, auto_archive_duration=10080)
+    await ctx.response.send_message(f"✅ {thread.mention}")
+
+
+@tree.command(name="rename-chall")
+async def rename_chall(ctx: discord.Interaction, category: Categories, problem_name: str):
+    channel_name = f"{category.name}: {problem_name}"
+    for channel in ctx.channel.threads:
+        if channel.name.removesuffix(SOLVED_POSTFIX) == channel_name:
+            await ctx.response.send_message(f"すでに存在するよ！", ephemeral=True)
+            return
+    await ctx.response.send_message("✅")
+    await ctx.channel.edit(name=channel_name)
+
+
+@tree.command(name="solved")
 async def solved(ctx: discord.Interaction):
-    print(ctx.channel)
+    if not isinstance(ctx.channel, Thread) or ctx.channel.category_id != config.contests_category_id:
+        await ctx.response.send_message(f"コンテストスレッドじゃないよ！", ephemeral=True)
+        return
+    if ctx.channel.name.endswith(SOLVED_POSTFIX):
+        await ctx.response.send_message(f"もうsolvedだよ！", ephemeral=True)
+        return
+    await ctx.response.send_message("Congratulations!")
+    await ctx.channel.edit(name=ctx.channel.name + SOLVED_POSTFIX)
+
+
+@tree.command(name="unsolved")
+async def unsolved(ctx: discord.Interaction):
+    if not isinstance(ctx.channel, Thread) or ctx.channel.category_id != config.contests_category_id:
+        await ctx.response.send_message(f"コンテストスレッドじゃないよ！", ephemeral=True)
+        return
+    if not ctx.channel.name.endswith(SOLVED_POSTFIX):
+        await ctx.response.send_message(f"まだsolvedじゃないよ！", ephemeral=True)
+        return
+    await ctx.response.send_message("✅")
+    await ctx.channel.edit(name=ctx.channel.name.removesuffix(SOLVED_POSTFIX))
--- boonbot/config.py
+++ boonbot/config.py
@@ -6,6 +6,7 @@ class Config:
 
     bot_channel_id: int = -1
     main_channel_name: str = ""
+    contests_category_id: int = 1166000686975172690
 
     member_role_ids: List[int] = []
     bot_role_ids: List[int] = []
