2540be001d15cc8466be16a311be22e55393d751
Yu <yu212.mc@gmail.com>
2023-10-24 02:45:56 +0900

    change const name

--- boonbot/main.py
+++ boonbot/main.py
@@ -15,7 +15,7 @@ client = discord.Client(intents=intents)
 tree = app_commands.CommandTree(client)
 logger = logging.getLogger(__name__)
 
-SOLVED_POSTFIX = " (solved)"
+SOLVED_SUFFIX = " (solved)"
 CHECK_EMOJI = "✅"
 ERROR_EMOJI = "😡"
 
