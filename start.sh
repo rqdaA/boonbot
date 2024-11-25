0482781691dadeae5144b1d0af97997aa058df06
rqdaA <83322694+rqdaA@users.noreply.github.com>
2024-11-25 10:53:14 +0900

    fix: do not quote list env var

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-python run.py --token "$TOKEN" --guild-id "$GUILD_ID" --bot-channel-id "$BOT_CHANNEL_ID" --contests-category-ids "$CONTESTS_ID" --team-role-ids "$TEAM_ID"
+python run.py --token "$TOKEN" --guild-id "$GUILD_ID" --bot-channel-id "$BOT_CHANNEL_ID" --contests-category-ids $CONTESTS_ID --team-role-ids $TEAM_ID --team-names $TEAM_NAMES
