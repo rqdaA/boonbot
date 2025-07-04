16ae148a4f68aedd9764f9fa63d8b23fc673f1e8
Kota Toda <83322694+rqdaA@users.noreply.github.com>
2025-07-04 16:53:23 +0900

    [fix] need_reactionでないCTFにreactionをつけた時に不正に参加できないように変更

--- boonbot/chall.py
+++ boonbot/chall.py
@@ -20,20 +20,27 @@ class JoinButton(View):
     @discord.ui.button(label="Join", style=discord.ButtonStyle.primary)
     async def join_button(self, interaction: discord.Interaction, button: Button):
         await self.thread.add_user(interaction.user)
-        await interaction.response.send_message(f"スレッドに参加しました {CHECK_EMOJI}", ephemeral=True)
+        await interaction.response.send_message(
+            f"スレッドに参加しました {CHECK_EMOJI}", ephemeral=True
+        )
 
 
 class AutoJoinButton(View):
     def __init__(self):
         super().__init__(timeout=None)
 
-    @discord.ui.button(label="問題スレッドに自動で参加", style=discord.ButtonStyle.success)
+    @discord.ui.button(
+        label="問題スレッドに自動で参加", style=discord.ButtonStyle.success
+    )
     async def auto_join_button(self, interaction: discord.Interaction, button: Button):
         channel_id = interaction.channel.id
         if channel_id not in auto_join_users:
             auto_join_users[channel_id] = set()
         auto_join_users[channel_id].add(interaction.user.id)
-        await interaction.response.send_message(f"問題スレッドに自動で参加するように設定しました {CHECK_EMOJI}", ephemeral=True)
+        await interaction.response.send_message(
+            f"問題スレッドに自動で参加するように設定しました {CHECK_EMOJI}",
+            ephemeral=True,
+        )
 
 
 class Categories(enum.Enum):
@@ -51,23 +58,28 @@ class Categories(enum.Enum):
 @tree.command(name="new-chall", description="問題スレッドを作成します")
 async def new_chall(ctx: discord.Interaction, category: Categories, problem_name: str):
     channel_name = f"{category.value}: {problem_name}"
-    if not await util.check_is_in_contest_channel(ctx) or not await util.check_channel_exists(
-        ctx, ctx.channel, channel_name
-    ):
+    if not await util.check_is_in_contest_channel(
+        ctx
+    ) or not await util.check_channel_exists(ctx, ctx.channel, channel_name):
         return
     await ctx.response.defer(ephemeral=True)
     thread = await ctx.channel.create_thread(
         name=channel_name, type=ChannelType.public_thread, auto_archive_duration=10080
     )
     await thread.send(view=JoinButton(thread))
-    adding_users = set(ctx.guild.get_member(user_id) for user_id in auto_join_users.get(ctx.channel.id) or [])
+    adding_users = set(
+        ctx.guild.get_member(user_id)
+        for user_id in auto_join_users.get(ctx.channel.id) or []
+    )
     for user in adding_users | {ctx.user}:
         await thread.add_user(user)
     await ctx.delete_original_response()
 
 
 @tree.command(name="rename-chall", description="問題名を変更します")
-async def rename_chall(ctx: discord.Interaction, category: Categories, problem_name: str):
+async def rename_chall(
+    ctx: discord.Interaction, category: Categories, problem_name: str
+):
     channel_name = f"{category.value}: {problem_name}"
     if not await util.check_is_in_thread(ctx) or not await util.check_channel_exists(
         ctx, ctx.channel.parent, channel_name
@@ -75,7 +87,11 @@ async def rename_chall(ctx: discord.Interaction, category: Categories, problem_n
         return
     await ctx.response.send_message(f"問題名を変更しました {CHECK_EMOJI}")
     await ctx.channel.edit(
-        name=(f"{SOLVED_PREFIX}{channel_name}" if ctx.channel.name.startswith(SOLVED_PREFIX) else channel_name)
+        name=(
+            f"{SOLVED_PREFIX}{channel_name}"
+            if ctx.channel.name.startswith(SOLVED_PREFIX)
+            else channel_name
+        )
     )
 
 
@@ -97,7 +113,9 @@ async def unsolved(ctx: discord.Interaction):
 
 
 @tree.command(name="new-ctf", description="新しいコンテストチャンネルを作成します")
-async def new_ctf(ctx: discord.Interaction, ctf_name: str, role_name: str, need_reaction: bool = False):
+async def new_ctf(
+    ctx: discord.Interaction, ctf_name: str, role_name: str, need_reaction: bool = False
+):
     if not await util.check_is_in_bot_cmd(ctx):
         return
     else:
@@ -120,17 +138,24 @@ async def new_ctf(ctx: discord.Interaction, ctf_name: str, role_name: str, need_
             overwrites=overwrites,
             position=0,
         )
-        await channel.send(f"team name: `{team_name}`\npassword: `{util.gen_password(16)}`", view=AutoJoinButton())
-        resp_text = f"{role_name}に{ctf_name} ({channel.mention}) を作成しました {CHECK_EMOJI}"
+        await channel.send(
+            f"team name: `{team_name}`\npassword: `{util.gen_password(16)}`",
+            view=AutoJoinButton(),
+        )
+        resp_text = (
+            f"{role_name}に{ctf_name} ({channel.mention}) を作成しました {CHECK_EMOJI}"
+        )
         if need_reaction:
             await ctx.response.defer()
-            msg = await ctx.followup.send(f"{resp_text}\n参加するには{JOIN_EMOJI}を押してください")
+            msg = await ctx.followup.send(
+                f"{resp_text}\n参加するには{JOIN_EMOJI}を押してください"
+            )
             await msg.add_reaction(JOIN_EMOJI)
         else:
             await ctx.response.send_message(resp_text)
 
 
-# TODO: add_everyone==FalseなCTFでunendをした時に閲覧制限の再設定がうまく行かない問題を解決
+# TODO: need_reaction==TrueなCTFでunendをした時に閲覧制限の再設定がうまく行かない問題を解決
 @tree.command(name="unend-ctf", description="コンテストの閲覧制限を再度付けます")
 async def unend_ctf(ctx: discord.Interaction):
     if not await util.check_is_in_contest_channel(ctx):
@@ -158,29 +183,43 @@ async def end_ctf(ctx: discord.Interaction):
             continue
         team_role = ctx.guild.get_role(util.get_role_by_category(ch.category))
         non_bot_member = list(filter(lambda u: not u.bot, ctx.guild.members))
-        whitelisted_members = list(filter(lambda u: ch.overwrites_for(u).read_messages, non_bot_member))
+        whitelisted_members = list(
+            filter(lambda u: ch.overwrites_for(u).read_messages, non_bot_member)
+        )
         await ch.edit(name=ch.name.lstrip(RUNNING_EMOJI))
         for member in whitelisted_members:
             await ch.set_permissions(member, overwrite=perm.PERMISSION_DEFAULT)
         await ch.set_permissions(team_role, overwrite=perm.PERMISSION_DEFAULT)
-        await ch.set_permissions(ctx.guild.default_role, overwrite=perm.PERMISSION_DEFAULT)
+        await ch.set_permissions(
+            ctx.guild.default_role, overwrite=perm.PERMISSION_DEFAULT
+        )
         if ch.id != ctx.channel.id:
             await ch.send(f"ロール制限を外しました {CHECK_EMOJI}")
     await ctx.response.send_message(f"ロール制限を外しました {CHECK_EMOJI}")
 
 
 @new_ctf.autocomplete("role_name")
-async def autocomplete(ctx: discord.Interaction, current: str) -> list[discord.app_commands.Choice[str]]:
-    return [app_commands.Choice(name=name, value=name) for name in config.team_names if current.lower() in name.lower()]
+async def autocomplete(
+    ctx: discord.Interaction, current: str
+) -> list[discord.app_commands.Choice[str]]:
+    return [
+        app_commands.Choice(name=name, value=name)
+        for name in config.team_names
+        if current.lower() in name.lower()
+    ]
 
 
 @client.event
-async def on_reaction_add(reaction: discord.Reaction, user: discord.Member | discord.User):
+async def on_reaction_add(
+    reaction: discord.Reaction, user: discord.Member | discord.User
+):
     emoji = reaction.emoji
     if emoji != JOIN_EMOJI:
         return
     message = reaction.message
     if not util.is_bot_cmd_channel(message.channel):
+        return
+    if not any(line.startswith("参加するには") for line in message.content.split("\n")):
         return
     if not any(message.content.startswith(role.name) for role in user.roles):
         return
@@ -191,12 +230,16 @@ async def on_reaction_add(reaction: discord.Reaction, user: discord.Member | dis
 
 
 @client.event
-async def on_reaction_remove(reaction: discord.Reaction, user: discord.Member | discord.User):
+async def on_reaction_remove(
+    reaction: discord.Reaction, user: discord.Member | discord.User
+):
     emoji = reaction.emoji
     if emoji != JOIN_EMOJI:
         return
     message = reaction.message
     if not util.is_bot_cmd_channel(message.channel):
+        return
+    if not any(line.startswith("参加するには") for line in message.content.split("\n")):
         return
     if not any(message.content.startswith(role.name) for role in user.roles):
         return
